0095e4354a0c9338c707a4e79a7e44768cebe4d8
chatbotbuilder <robotbuilder@chatbots.studio>
2022-11-02 12:18:33 +0200

    save progress

---
+++ src/components/navigation/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './style.css';
+
+function Navigation () {
+ return (
+  <nav>
+   <div>Milton</div>
+   <hr />
+   <div>Eiusmod do anim irure consequat adipisicing ipsum ex.</div>
+  </nav>
+ );
+}
+
+export default Navigation;
