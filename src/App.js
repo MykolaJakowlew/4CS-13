bbfe5a2da8a15e7744d28c5fc99495ef176642a8
chatbotbuilder <robotbuilder@chatbots.studio>
2022-11-11 12:42:53 +0200

    save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Navigation from './components/navigation';
-import Content from './components/content';
-import PageNotFound from './components/404';
-import About from './components/about';
 
 function App () {
   return (
-    <BrowserRouter>
       <div className="App">
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Content />} >
-            <Route path='/card/:cardId' element={<Content />} />
-          </Route>
-          <Route path='/about' element={<About />} />
-
-          <Route path='*' element={<PageNotFound />} />
-        </Routes>
+        
       </div>
-    </BrowserRouter>
   );
 }
 
