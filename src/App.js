c098b78aa82472a2768740947b6e27513b1b8a9e
chatbotbuilder <robotbuilder@chatbots.studio>
2022-11-18 12:34:54 +0200

    clean

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 
 import './App.css';
-import Menu from './components/menu';
-import MyCarousel from './components/carousel';
 
 function App () {
   return (
     <div className="App">
-      <Menu />
-      <MyCarousel />
     </div>
   );
 }
